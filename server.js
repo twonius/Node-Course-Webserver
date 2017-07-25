5cdeac69a4f0ecd038a676b6824fdd5ac04b7b68
Anton Maes <antonemaes@gmail.com>
2017-07-24 21:44:48 -0700

    fixing server port

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
 var app = express();
 
 hbs.registerPartials(__dirname + '/views/partials')
@@ -53,5 +53,5 @@ app.get('/bad', (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log('Server is up on port ${port}');
+  console.log(`Server is up on port ${port}`);
 });
